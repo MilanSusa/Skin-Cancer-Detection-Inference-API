7e97379fd77ae3637f3b5ad4271a77b2fb97e6c1
Milan <milan_susa@hotmail.com>
2020-04-25 16:48:23 +0200

    Add file upload support

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+import os
+
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './static/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
-@app.route('/')
-def hello_world():
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/api/v1/inference', methods=['POST'])
+def perform_inference():
+    file = request.files['file']
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
     return 'Hello World!'
 
 
