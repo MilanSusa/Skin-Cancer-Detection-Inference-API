60bd34f2070bafcfcf7f4bc8abc8321b3e102a45
Milan <milan_susa@hotmail.com>
2020-04-25 17:09:32 +0200

    Add model loading

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, request
 from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model
+from tensorflow.keras.metrics import top_k_categorical_accuracy
 
 UPLOAD_FOLDER = './static/images'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -9,6 +11,21 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def top_3_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=3)
+
+
+def top_2_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=2)
+
+
+MODEL = load_model('./pretrained_models/mobilenet.h5',
+                   custom_objects={
+                       'top_2_accuracy': top_2_accuracy,
+                       'top_3_accuracy': top_3_accuracy
+                   })
 
 
 def allowed_file(filename):
