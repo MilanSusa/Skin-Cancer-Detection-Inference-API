3b8863263d8bd2eb5bfa4e31bfae2d62889a8e33
Milan <milan_susa@hotmail.com>
2020-04-25 17:17:16 +0200

    Add inference response

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 from tensorflow.keras.metrics import top_k_categorical_accuracy
+from keras_preprocessing.image import ImageDataGenerator
+from keras.applications.mobilenet import preprocess_input
 
 UPLOAD_FOLDER = './static/images'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -40,7 +42,23 @@ def perform_inference():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-    return 'Hello World!'
+        test_img_gen = ImageDataGenerator(preprocessing_function=preprocess_input)
+        test_data_gen = test_img_gen.flow_from_directory(directory='static',
+                                                         target_size=(224, 224),
+                                                         color_mode='rgb')
+
+        preds = MODEL.predict_generator(generator=test_data_gen,
+                                        steps=1)
+
+    return jsonify({
+        "akiec": str(preds[0][0]),
+        "bcc": str(preds[0][1]),
+        "bkl": str(preds[0][2]),
+        "df": str(preds[0][3]),
+        "mel": str(preds[0][4]),
+        "nv": str(preds[0][5]),
+        "vasc": str(preds[0][6])
+    })
 
 
 if __name__ == '__main__':
